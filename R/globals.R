91416fcc045a9423806af382b0918687313e18f2
Fusaroli Michele <michele.fusaroli@gmail.com>
2024-03-13 11:23:32 +0100

    reference fixed

--- R/globals.R
+++ R/globals.R
@@ -4,7 +4,7 @@ utils::globalVariables(c("primaryid"))
   packageStartupMessage("Welcome to DiAna package. A tool for standardized, flexible, and transparent disproportionality analysis on the FAERS.")
   packageStartupMessage(paste0("To cite package ‘DiAna’ in publications use:
 
-  Fusaroli M, Giunchi V (",packageDate("DiAna"),"). DiAna version ", packageVersion("DiAna"), " Advanced
+  Fusaroli M., Giunchi V. - DiAna version ", packageVersion("DiAna")," (",packageDate("DiAna"), "). Advanced
   Disproportionality Analysis in the FAERS for Drug
   Safety.
   https://github.com/fusarolimichele/DiAna_package,
