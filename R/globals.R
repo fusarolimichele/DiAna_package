25fc20eb80eea07240df5fae0dbafa3eac1a16c4
Fusaroli Michele <michele.fusaroli@gmail.com>
2024-05-23 13:50:40 +0200

    fix startup message

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,10 @@ utils::globalVariables(c("primaryid"))
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to DiAna package. A tool for standardized, flexible, and transparent pharmacovigilance analyses on the FAERS.")
+  packageStartupMessage()
   packageStartupMessage(paste0("To cite this package in your work and publications use: ", DiAna_reference()[2]))
+  packageStartupMessage()
   packageStartupMessage(DiAna_reference()[4])
+  packageStartupMessage()
   packageStartupMessage(DiAna_reference()[6])
 }
